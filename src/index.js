a49ee49565c25d730cc98abdac2bef14d6c41ab8
Lourdes Bernales <lobernales@hotmail.com>
2023-01-11 20:11:37 -0500

    commit ejemplo

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const fn= () => {
+    const mistring = "soy un String"
+    let b= mistring + " muy feliz"
+    console.log(b)
+} 
+fn()
+const clicleando= () => {
+    let a=  " chanchita feliz"
+    console.log(a)
+} 
+
